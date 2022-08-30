071fe6df46256111cf07e741fdcbc870c65db23e
Leandro Simeao <leandrosimeao@yahoo.com.br>
2022-08-30 09:51:00 -0300

    First Commit

---
+++ src/Componentes/CampoTexto/index.jsx
@@ -0,0 +1,20 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) =>{
+
+    let valor = '';
+
+    const aoDigitado = (e) => {
+        valor = e.target.value;
+        console.log(valor)
+    }
+
+    return(
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+        </div>
+    )
+}
+
+export default CampoTexto
